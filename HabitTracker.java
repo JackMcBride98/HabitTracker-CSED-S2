7d3f1893b8cb1d22381ee1eef76929a72180aea0
Muccy98 <37148941+Muccy98@users.noreply.github.com>
2018-03-14 12:45:29 +0000

    Add the ability to save habits.
    Habits can now be saved and are loaded with each use of the program.

--- HabitTracker.java
+++ HabitTracker.java
@@ -1,14 +1,21 @@
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-// This handles the GUI aspect of the program.
+
 public class HabitTracker extends JFrame implements ActionListener{
 	
 	private JPanel addHabitPanel;
@@ -29,10 +36,62 @@ public class HabitTracker extends JFrame implements ActionListener{
 		addHabitPanel.add(addHabitTextField);
 		addHabitPanel.add(addHabitButton);
 		add(addHabitPanel);
+		loadHabits();
+		displayHabits();
 		pack();
 		setVisible(true);
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				saveHabits();
+			}
+		});	
  	}
 
+	public void saveHabits() {
+		try {
+			PrintWriter pw = new PrintWriter(new FileWriter("Data.txt"));
+			for(Habit h: habits) {
+				pw.println(h.getName());
+			}
+			pw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void save(String habit) {
+		try {
+			PrintWriter pw = new PrintWriter(new FileWriter("Data.txt"));
+			pw.println(habit);
+			pw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void displayHabits() {
+		for(Habit h: habits) {
+			add(h);
+		}
+	}
+	
+	public void loadHabits() {
+		try {
+			FileReader fileReader = new FileReader("Data.txt");
+			BufferedReader br = new BufferedReader(fileReader);
+			String savedHabit;
+			while( (savedHabit = br.readLine()) != null ) {
+				habits.add(new Habit(savedHabit));
+			}
+			br.close();
+			fileReader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException er) {
+			er.printStackTrace();
+		}
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e ) {
 		if (addHabitTextField.getText() != null) {
@@ -48,3 +107,4 @@ public class HabitTracker extends JFrame implements ActionListener{
 		HabitTracker habitTracker = new HabitTracker();
 	}
 }
+
